fc7f0e6ecb9102ddfe8aa942a7df5b2fa2386a5e
joebrooks <lcw3176@gmail.com>
2021-01-17 23:35:30 +0900

    test view

---
+++ js/map.js
@@ -0,0 +1,18 @@
+var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지도를 표시할 div  
+    staticMapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 이미지 지도의 중심좌표
+        mapTypeId: kakao.maps.MapTypeId.SKYVIEW,
+        level: 3 // 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); 
