66d95bfb6cc87476de9f47c9b3d4e7e64b4c7c70
joebrooks <lcw3176@naver.com>
2021-01-18 10:45:31 +0900

    download test

--- js/map.js
+++ js/map.js
@@ -7,16 +7,31 @@ var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지
 
 // 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
 var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+var filename = 'image.png'
+var data;
 
 window.onload = function (){
     var sm = document.getElementById('staticMap');
     var img =sm.getElementsByTagName("img");
-    var link = document.getElementById("downloadLink");
-    link.href = img[0].src;
-
-    var imagetag = document.getElementById("downloadImage");
-    imagetag.src = img[0].src;
+    data = img[0].src;
 }
+
+
+function downloadImage(){
+    if (window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(data, fileName);
+        return;
+      }
+      
+      const blob = new Blob([data], { type: 'image/png' })
+      const path = window.URL.createObjectURL(data)
+      const link = document.createElement('a')
+      link.href = path
+      link.download = fileName
+      link.click()
+      link.remove()
+}
+
 
 
 // var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
