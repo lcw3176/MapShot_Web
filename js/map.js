7f78aac37f2f211ec5fcc69a911a0cb314eeab74
joebrooks <lcw3176@naver.com>
2021-01-18 09:44:11 +0900

    hello

--- js/map.js
+++ js/map.js
@@ -8,6 +8,15 @@ var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지
 // 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
 var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
 
+function downloadImage(){
+    var sm = document.getElementById('staticMap');
+    var img =sm.getElementsByTagName("img");
+    var button = document.getElementById("downloadButton");
+    button.href = img[0].src;
+    button.download = "이미지";
+    button.click();
+
+}
 // var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 //     mapOption = { 
 //         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
