81d28897709cf4078b32b0764f79f0c3a671b6fd
joebrooks <lcw3176@naver.com>
2021-01-19 09:14:33 +0900

    fix map loading

--- js/map.js
+++ js/map.js
@@ -6,14 +6,18 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         level: 3 // 지도의 확대 레벨
     };  
 
-// 지도를 생성합니다    
-var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-// 장소 검색 객체를 생성합니다
-var ps = new kakao.maps.services.Places();  
+window.onload = function(){
+    // 지도를 생성합니다    
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-// 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
-var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+    // 장소 검색 객체를 생성합니다
+    var ps = new kakao.maps.services.Places();  
+
+    // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+}
+
 
 // 키워드 검색을 요청하는 함수입니다
 function searchPlaces() {
