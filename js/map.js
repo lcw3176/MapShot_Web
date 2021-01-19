e0799ab04b1de28f031ae7995c82fcb16f1ab061
joebrooks <lcw3176@naver.com>
2021-01-19 12:15:06 +0900

    fix callback argument error

--- js/map.js
+++ js/map.js
@@ -65,7 +65,7 @@ function searchAddrFromCoords(coords, callback) {
 
 function searchDetailAddrFromCoords(coords, callback) {
     // 좌표로 법정동 상세 주소 정보를 요청합니다
-    geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    geocoder.coord2Address(coords.getLng(), coords.getLat(), null);
 }
 
 
