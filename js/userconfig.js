75b418eaad9a4c064434492f275036e3d49c3eba
joebrooks <lcw3176@naver.com>
2021-01-19 17:56:27 +0900

    image test

--- js/userconfig.js
+++ js/userconfig.js
@@ -49,16 +49,16 @@ function startCapture(){
     if(checkValue()){
         
         container  = document.createElement('div'); // 이미지 지도를 표시할 div  
-        container.style.width = 1000 +"px";
-        container.style.height = 1000 +"px";
+        container.style.width = 100 +"px";
+        container.style.height = 100 +"px";
 
-        staticMapOption = { 
+        MapOption = { 
             center: new kakao.maps.LatLng(lat, lng), // 이미지 지도의 중심좌표
             level: 3 // 이미지 지도의 확대 레벨
         };
 
         // 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
-        var map = new kakao.maps.StaticMap(container, staticMapOption);
+        var map = new kakao.maps.StaticMap(container, MapOption);
 
         var aTag = document.getElementById('downloadLink');
         aTag.src = container.lastElementChild.lastElementChild.src;
