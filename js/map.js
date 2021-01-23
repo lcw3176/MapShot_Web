88adbefadb03394c17d0b5f58454d2f812c52c53
joebrooks <lcw3176@naver.com>
2021-01-23 22:46:12 +0900

    test

--- js/map.js
+++ js/map.js
@@ -22,16 +22,6 @@ window.onload = function(){
         return;
     }
 
-    var filter = "win16|win32|win64|mac|macintel"; 
-    
-    if ( navigator.platform ) 
-    { 
-        if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
-            alert('모바일 환경에서는 배율이 1단계로 고정됩니다. 원활한 사용을 원하시면 pc로 접속해 주세요.'); 
-            isMobile = true;
-        } 
-    }
-
 
     // 지도를 생성합니다    
     map = new kakao.maps.Map(mapContainer, mapOption); 
