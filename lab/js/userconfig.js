3c184a5c76a504e396bff6e3003fc1f674475e52
joebrooks <lcw3176@naver.com>
2021-01-30 22:51:35 +0900

    test

--- lab/js/userconfig.js
+++ lab/js/userconfig.js
@@ -124,7 +124,6 @@ function getHelpLine(x, y){
     
     var tempCenterLat = Number(centerLat) - ((moveYPosition * blockWidth) * (y - 1));
     var tempCenterLng = Number(centerLng) + ((moveXPosition * blockWidth) * (x - 1));
-
     
     var sw = new kakao.maps.LatLng(
         tempCenterLat - (moveYPosition * parseInt(blockWidth / 2)), 
