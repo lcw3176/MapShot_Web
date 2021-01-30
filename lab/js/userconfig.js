b6b56458de4340656ed626a21cb524168844b01e
joebrooks <lcw3176@naver.com>
2021-01-30 16:35:41 +0900

    test

--- lab/js/userconfig.js
+++ lab/js/userconfig.js
@@ -130,10 +130,10 @@ function getHelpLine(x, y){
     var centerNeY = Lat + (moveYPostion / 2);
     var centerNeX = Lng + (moveXPosition * blockWidth) + (moveXPosition / 2);
 
-    centerSwY = centerSwY + ((y - 2) * ((moveYPostion * blockWidth) - (moveYPostion / 2)));
-    centerSwX = centerSwX + ((x - 2) *  (Lng - (moveXPosition / 2)));
-    centerNeY = centerNeY - ((y - 2) *  (Lat - (moveYPostion / 2)));
-    centerNeX = centerNeX - ((x - 2) * ((moveXPosition * blockWidth) - (moveXPosition / 2)));
+    centerSwY = centerSwY - ((y - 2) * ((moveYPostion * blockWidth) - (moveYPostion / 2)));
+    centerSwX = centerSwX - ((x - 2) *  (Lng - (moveXPosition / 2)));
+    centerNeY = centerNeY + ((y - 2) *  (Lat - (moveYPostion / 2)));
+    centerNeX = centerNeX + ((x - 2) * ((moveXPosition * blockWidth) - (moveXPosition / 2)));
     // var xWidth = centerSwX - centerNeX;
     // var yWidth = centerNeY - centerSwY;
     
