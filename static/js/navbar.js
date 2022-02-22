d3d38f1cc3c5e15f4427d99e7ea1d10eb1ebf5cf
joebrooks <lcw3176@naver.com>
2022-02-22 17:03:56 +0900

    애드센스 관련 추가

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -5,6 +5,10 @@ window.addEventListener("load", function () {
     if (this.status == 200 && this.readyState == this.DONE) {
       document.getElementById("navbar").innerHTML = xml.responseText;
     }
+
+    if(this.readyState == this.DONE){
+      loadGoogleAds();
+    }
   };
 
   xml.open("GET", "../static/template/navbar.html", true);
@@ -20,4 +24,13 @@ window.addEventListener("load", function () {
       document.getElementById("navbar-menu").setAttribute("class", "navbar-menu");
     }
   };
+
+  function loadGoogleAds() {
+    var scriptTag = document.createElement('script');
+    scriptTag.setAttribute("data-ad-client", "ca-pub-7390022674285155");
+    scriptTag.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+    scriptTag.setAttribute('type', 'text/javascript');
+    scriptTag.setAttribute('async', 'async');
+    document.body.appendChild(scriptTag);
+  }
 });
